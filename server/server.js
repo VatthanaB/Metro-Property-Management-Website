52f64f56bf2dcd6a63a582cf8dd2c622682c20f4
Vatthana Boulom <boulom.vatthana.pro@gmail.com>
2023-12-11 20:40:39 +1300

    Update MongoDB URI in docker-compose.yml and server.js

--- server/server.js
+++ server/server.js
@@ -6,14 +6,13 @@ const greetingRoutes = require("./routes/vatthana/greetingRoutes");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
-dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
-
-const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://mongodb:27017/Mission-Ready-M5";
 app.use(cors());
 app.use(bodyParser.json());
+
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://mongodb:27018/Mission-Ready-M5";
 
 // MongoDB connection
 mongoose.connect(MONGODB_URI, {
