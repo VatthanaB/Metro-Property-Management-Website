ac17882e910913f4c854b88d5fd83b65782a915d
Vatthana Boulom <boulom.vatthana.pro@gmail.com>
2023-12-12 22:15:14 +1300

    Add seeding during startup in docker-compose.yml

---
+++ server/cli/cli.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const { MongoClient } = require("mongodb");
+const program = require("commander");
+const dotenv = require("dotenv");
+dotenv.config();
+
+program
+  .version("1.0.0")
+  .description("CLI tool to create MongoDB database locally from a JSON file");
+
+program
+  .command("import <jsonFilePath> <databaseName> <collectionName>")
+  .description("Import data from JSON file to MongoDB database")
+  .action(async (jsonFilePath, databaseName, collectionName) => {
+    try {
+      const jsonData = JSON.parse(fs.readFileSync(jsonFilePath, "utf8"));
+
+      const client = new MongoClient(process.env.MONGODB_SEEDING);
+      await client.connect();
+
+      const db = client.db(databaseName);
+      const collection = db.collection(collectionName);
+
+      await collection.insertMany(jsonData);
+
+      console.log("Data imported successfully.");
+
+      await client.close();
+    } catch (error) {
+      console.error("Error:", error.message);
+    }
+  });
+
+// Run this command in the terminal to import the data from the JSON file to the database:
+// node ./cli/cli.js import ./cli/property.json Mission-Ready-M5 properties
+
+// program .parse method parses process.argv and calls the action handler
+program.parse(process.argv);
