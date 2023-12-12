705c4efaf5133b3e58aebef31aa491f91b70aede
Vatthana Boulom <boulom.vatthana.pro@gmail.com>
2023-12-13 00:11:43 +1300

    Update API endpoint and display first property data

--- client/src/pages/vatthana/Homepage.jsx
+++ client/src/pages/vatthana/Homepage.jsx
@@ -5,15 +5,17 @@ const Homepage = () => {
   const [greetings, setGreetings] = useState("Hello World");
 
   useEffect(() => {
-    axios.get("http://localhost:5000/greetings").then((res) => {
+    axios.get("http://localhost:5000/properties/all").then((res) => {
       console.log(res.data);
       setGreetings(res.data);
     });
   }, []);
   return (
     <div className="flex flex-col items-center justify-start min-h-screen mt-40">
-      <h1 className="text-5xl my-4 text-gray-700">Greetings from the Team :</h1>
-      <h2 className="text-4xl text-gray-700">{greetings[0].message}</h2>
+      <h1 className="text-5xl my-4 text-gray-700">
+        Datas from first property :
+      </h1>
+      <h2 className="text-4xl text-gray-700">{greetings[0].address}</h2>
     </div>
   );
 };
