728432bd37ee9ba9524f5eb2d8dbbafc233e6021
Vatthana Boulom <boulom.vatthana.pro@gmail.com>
2023-12-14 15:55:36 +1300

    Refactor routing and add pagination to ResultPage

--- client/src/pages/ResultPage/ResultPage.jsx
+++ client/src/pages/ResultPage/ResultPage.jsx
@@ -1,5 +1,4 @@
-// ResultPage.js
-
+// Import necessary dependencies and components
 import { useRouteLoaderData } from "react-router-dom";
 import PropertyCard from "./components/PropertyCard";
 import { filterData } from "../../Utils/filteringHandler.js";
@@ -7,41 +6,51 @@ import { useFilters } from "../../Utils/contexts/FilterManagementContext";
 import { useEffect, useState } from "react";
 import { usePagination } from "../../Utils/paginationUtils.js"; // Import the pagination utility
 
+// ResultPage component to display filtered properties with pagination
 const ResultPage = () => {
+  // State to store filtered properties
   const [propertiesToDisplay, setPropertiesToDisplay] = useState([]);
+
+  // Fetch route loader data and filters using custom hooks
   const data = useRouteLoaderData("root");
   const { filters, updateFilters } = useFilters();
 
-  // UseEffect to filter data based on filters and update the propertiesToDisplay
+  // UseEffect to filter data based on filters and update propertiesToDisplay
   useEffect(() => {
     const filteredData = filterData(data, filters);
     setPropertiesToDisplay(filteredData);
   }, [data, filters, updateFilters]);
 
-  console.log(propertiesToDisplay);
-
-  // Use the pagination utility
+  // Use the pagination utility to manage pagination logic
   const { currentPage, currentItems, pageNumbers, handleClick, scrollToTop } =
     usePagination(propertiesToDisplay);
 
+  // Return JSX to render the component
   return (
     <div className="flex justify-center items-center mt-10">
       <div>
+        {/* Header */}
         <h1 className="text-gray-500 text-lg ml-8 mb-10 ">
           Home Page {">"} Rental Properties {">"} Search Results
         </h1>
         <h1 className="text-gray-500 text-3xl ml-8 ">Browse Properties</h1>
+
+        {/* Display filtered properties */}
         <div className="flex flex-col space-y-5">
           {currentItems.map((property) => (
             <PropertyCard key={property.id} {...property} />
           ))}
         </div>
+
+        {/* Back to Top Button */}
         <button
           className="cursor-pointer px-4 py-2 mx-auto flex justify-center items-center text-white bg-gray-600 rounded-md mt-5 font-light"
           onClick={scrollToTop}
         >
           Back to Top
         </button>
+
+        {/* Pagination Numbers */}
         <div className="flex justify-center mt-5">
           {pageNumbers.map((number) => (
             <div
@@ -60,4 +69,5 @@ const ResultPage = () => {
   );
 };
 
+// Export the ResultPage component
 export default ResultPage;
