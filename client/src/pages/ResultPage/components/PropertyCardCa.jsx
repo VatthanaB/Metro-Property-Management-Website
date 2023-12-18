4b75773c3a343052f37695055db729f1b196bbb0
Vatthana Boulom <boulom.vatthana.pro@gmail.com>
2023-12-18 13:16:58 +1300

    Refactor PropertyCardCa component

--- client/src/pages/ResultPage/components/PropertyCardCa.jsx
+++ client/src/pages/ResultPage/components/PropertyCardCa.jsx
@@ -1,18 +1,10 @@
 /* eslint-disable react/prop-types */
 import { MdOutlineLocationOn } from "react-icons/md";
-import { FaDollarSign } from "react-icons/fa";
-import { MdOutlineBed } from "react-icons/md";
-import { MdOutlineShower } from "react-icons/md";
-import { GiHomeGarage } from "react-icons/gi";
-import {
-  CustomNextArrow,
-  CustomPrevArrow,
-} from "./propertyCardArrows/Slidearrows";
-import { Link } from "react-router-dom";
-import Slider from "react-slick";
+
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
+import CardCarroussel from "./propertCardcomponent/CardCarroussel";
+import PriceAndAmenities from "./propertCardcomponent/PriceAndAmenities";
 // PropertyCard component to display property details
 const PropertyCard = (property) => {
   // Check if the screen size is mobile
@@ -27,55 +19,10 @@ const PropertyCard = (property) => {
       </p>
 
       {/* Image and Arrows */}
-      <div className="relative text-red-600 text-6xl">
-        <Slider
-          dots={false}
-          infinite={true}
-          speed={650}
-          slidesToShow={1}
-          slidesToScroll={1}
-          nextArrow={<CustomNextArrow />}
-          prevArrow={<CustomPrevArrow />}
-        >
-          {carrouselImages.map((image, index) => (
-            <Link key={index} to={`/property/${property._id}`}>
-              <img
-                className="w-full h-[450px] md:h-[400px] object-cover rounded md:rounded-t-xl"
-                src={`/images/property/${image}.jpeg`}
-                alt={property.type}
-              />
-            </Link>
-          ))}
-        </Slider>
-      </div>
+      <CardCarroussel carrouselImages={carrouselImages} property={property} />
 
       {/* Price / Bed / Shower / Garage */}
-      <div className=" flex-col md:flex justify-between pt-5">
-        <p className="md:hidden flex text-2xl pl-5 pb-3 space-x-2">
-          <MdOutlineLocationOn className="text-red-600" />
-          <p className="font-extralight text-base">{property.address}</p>
-        </p>
-        <p className="flex text-2xl pl-5 pb-3 items-center font-light">
-          <FaDollarSign className="text-red-600" />
-          <p>
-            {property.price} <span className="text-lg">per week</span>
-          </p>
-        </p>
-        <div className="space-x-10 flex justify-center items-center text-base pr-5">
-          <div className="flex">
-            <MdOutlineBed className="mr-4 text-2xl" />
-            {property.bedrooms}
-          </div>
-          <div className="flex">
-            <MdOutlineShower className="mr-4 text-2xl" />
-            {property.bathrooms}
-          </div>
-          <div className="flex">
-            <GiHomeGarage className="mr-4 text-2xl" />
-            {property.parking}
-          </div>
-        </div>
-      </div>
+      <PriceAndAmenities property={property} />
 
       {/* Available / Maximum Tenants */}
       <div className="  md:flex justify-between my-3 text-gray-600 font-normal">
