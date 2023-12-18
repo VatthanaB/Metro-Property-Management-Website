0323ec1240336acd687092f0fb3926e927b62c53
Jason Cluley <jaxeus@gmail.com>
2023-12-18 23:22:47 +1300

    Completed the drop-down div for showing amenities and vicinity in mobile version. Also amended text-lg for available and max-tenants.

--- client/src/pages/ResultPage/components/propertyCardComponents/AmenitiesAndVicinity.jsx
+++ client/src/pages/ResultPage/components/propertyCardComponents/AmenitiesAndVicinity.jsx
@@ -1,13 +1,61 @@
+import { useState } from "react";
+
 /* eslint-disable react/prop-types */
 const AmenitiesAndVicinity = ({ property }) => {
+  const [showMobileDiv, setShowMobileDiv] = useState(false);
+
+  function toggleMobileDiv() {
+    setShowMobileDiv(!showMobileDiv);
+  }
+
+  const mobileDiv = (
+    <>
+      <div className="md:hidden flex  space-x-10 justify-start items-center">
+        <ul className="flex space-x-0 font-extralight flex-wrap items-end space-y-0 gap-4 -mx-2">
+          <li className="text-lg font-normal">Amenities</li>
+          {property.amenities.map((amenity, index) => (
+            <li
+              className="rounded-3xl px-2 py-0.5 border-2 border-gray-300 self-center text-sm"
+              key={index}
+            >
+              {amenity}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <hr className="md:hidden border-1 border-gray-300 w-full my-2 -mx-3" />
+      <div className="md:hidden flex space-x-10 justify-start items-center">
+        <ul className="flex space-x-0 font-extralight flex-wrap items-end space-y-0 gap-4 -mx-2">
+          <li className="text-lg font-normal">Vicinity</li>
+          {property.vicinity.map((vicinity, index) => (
+            <li
+              className="rounded-3xl px-2 py-0.5 border-2 border-gray-300 self-center text-sm"
+              key={index}
+            >
+              {vicinity}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <p
+        onClick={toggleMobileDiv}
+        className="flex md:hidden text-red-500 underline justify-center "
+      >
+        Show Less
+      </p>
+    </>
+  );
+
   return (
     <div className="  md:block pl-5 space-y-4">
-      <div className=" flex md:hidden text-red-500 underline justify-center">
-        <p>See amenities & Vicinity</p>
-      </div>
+      {!showMobileDiv && (
+        <div className=" flex md:hidden text-red-500 underline justify-center">
+          <p onClick={toggleMobileDiv}>See amenities & Vicinity</p>
+        </div>
+      )}
       <div className="hidden md:flex  space-x-10 justify-start items-center">
-        <p className="text-xl">Amenities</p>
         <ul className="flex space-x-3 font-extralight">
+          <p className="text-xl">Amenities</p>
           {property.amenities.map((amenity, index) => (
             <li
               className="rounded-3xl px-2 py-1 border-2 border-gray-300 self-center"
@@ -18,6 +66,7 @@ const AmenitiesAndVicinity = ({ property }) => {
           ))}
         </ul>
       </div>
+
       <div className="hidden md:flex space-x-10 justify-start items-center">
         <p className="text-xl">Vicinity</p>
         <ul className="flex space-x-3 font-extralight">
@@ -32,6 +81,7 @@ const AmenitiesAndVicinity = ({ property }) => {
         </ul>
       </div>
       {/* MOBILE */}
+      {showMobileDiv && mobileDiv}
     </div>
   );
 };
--- client/src/pages/ResultPage/components/propertyCardComponents/AvailableAndMaximumTenants.jsx
+++ client/src/pages/ResultPage/components/propertyCardComponents/AvailableAndMaximumTenants.jsx
@@ -2,7 +2,7 @@ const AvailableAndMaximumTenants = () => {
   return (
     <div className="  md:flex justify-between my-3 text-{fontColor}-600 font-normal ">
       <div className="md:flex-col md:w-1/2 px-2">
-        <div className="flex justify-between w-full px-5">
+        <div className="flex justify-between w-full px-5 text-lg">
           <p>Available</p>
           <p> Tue, 12 Dec 2023</p>
         </div>
