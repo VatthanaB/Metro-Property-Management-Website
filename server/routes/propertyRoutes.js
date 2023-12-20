3e6ec7d89b5161d85b263c0397e0e4184dcaaf97
Vatthana Boulom <boulom.vatthana.pro@gmail.com>
2023-12-20 16:21:05 +1300

    Refactor propertyRoutes.js: Remove unused code

--- server/routes/propertyRoutes.js
+++ server/routes/propertyRoutes.js
@@ -23,54 +23,6 @@ router.get("/all", async (req, res) => {
   }
 });
 
-// Endpoint: Add a new property
-router.post("/add", async (req, res) => {
-  try {
-    // Extract property details from the request body
-    const {
-      image,
-      address,
-      type,
-      location,
-      description,
-      price,
-      bedrooms,
-      bathrooms,
-      parking,
-      amenities,
-      vicinity,
-    } = req.body;
-
-    // Check if all required fields are provided
-    if (!image || !address || !type || !location || !description || !price) {
-      return res.status(400).json({ error: "All fields are required" });
-    }
-
-    // Create a new Property instance
-    const newProperty = new Property({
-      image,
-      address,
-      type,
-      location,
-      description,
-      price,
-      bedrooms,
-      bathrooms,
-      parking,
-      amenities,
-      vicinity,
-    });
-
-    // Save the new property to the database
-    const savedProperty = await newProperty.save();
-    res.status(201).json(savedProperty);
-  } catch (error) {
-    // Handle errors and send an appropriate response
-    console.error(error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
-
 // Endpoint: Add properties from a JSON file
 router.post("/json", async (req, res) => {
   try {
